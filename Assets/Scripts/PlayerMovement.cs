92c1d1016c35100a3c25474d1a60a9eaf37d377a
Laboratorio <laboratorio@udla-ec.int>
2025-05-21 20:41:42 -0500

    pacman escenario

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveX = Input.GetAxis("Horizontal"); // A/D
+        float moveZ = Input.GetAxis("Vertical");   // W/S
+
+        Vector3 movement = new Vector3(moveX, 0, moveZ);
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
